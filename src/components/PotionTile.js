3b30d0b8ca5f87129edad28cd233f78f4438c33b
Steven Mentzer <stevenfmentzer@gmail.com>
2023-12-18 11:12:56 -0500

    done

--- src/components/PotionTile.js
+++ src/components/PotionTile.js
@@ -8,8 +8,16 @@ Consider what the tiles have in common and what they do not:
 - For things that are different, consider how you might make use of props and JSX to have the same component behave differently when rendered by different parents 
 */
 
-function PotionTile(props) {
-  return <div>PotionTile</div>;
+function PotionTile({potion, buySell, children}) {
+
+  return(
+      <div className="card">
+      <div onClick={() => buySell(potion.id)} className="image-wrapper">
+        <img className="image" alt={potion.name} src={potion.image_url} />
+      </div>
+      {children}
+    </div>
+  )
 }
 
 export default PotionTile;
